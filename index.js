f794cc00c2f2d9ec4e9168f3f9478a033ac4eadb
michaelmirhom <micharby73@gmail.com>
2023-04-08 19:23:04 -0500

    michael

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+const h1 = document.createElement('h1');
+h1.textContent = 'newHeader';
+document.body.appendChild(h1);
+h1.id="victory";
+h1.textContent="YOUR-NAME is the champion";
+const victory = h1.textContent.replace('YOUR-NAME','Mirhom');
+const newHeader = document.querySelector('#victory');
+
